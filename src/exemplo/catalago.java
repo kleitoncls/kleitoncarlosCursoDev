79f57b4d2c33022fc24dfa89bb4a372e5800fa72
kleitoncls <kleitoncls@hotmail.com>
2019-02-21 20:02:15 -0300

    aula do adonias de teste

--- src/exemplo/catalago.java
+++ src/exemplo/catalago.java
@@ -4,12 +4,30 @@ public class catalago {
     public  static void main (String[] args){
         /*Estrutura de seleção*/
        int a=10, b=90,c=-100;
-       if(a>0) {
-           a = c;
+       if(c>a) {
+           c = a;
+       }else {
+           c = b;
        }
-       System.out.println(a);
-       }
-
-
+        System.out.println(c);
+       /*swith*/
+        int op=3
+       int v=0
+               switch(op){
+                   case 1:
+                       v=1;
+                       break;
+                       case2:
+                   v=2
+                           break;
+                   default:
+                       v=-1
+               }
+               System.out.println(v);
     }
+
+       }
+
+
+
 
