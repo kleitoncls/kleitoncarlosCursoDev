4c953d7626b8be256497066b6fec62c69ec4933b
kleitoncls <kleitoncls@hotmail.com>
2019-02-21 19:50:47 -0300

    Initial commit

---
+++ src/exemplo/catalago.java
@@ -0,0 +1,15 @@
+package exemplo;
+
+public class catalago {
+    public  static void main (String[] args){
+        /*Estrutura de seleção*/
+       int a=10, b=90,c=-100;
+       if(a>0) {
+           a = c;
+       }
+       System.out.println(a);
+       }
+
+
+    }
+
