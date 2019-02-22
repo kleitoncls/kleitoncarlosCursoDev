9ff9da93e496b2774ff07917ac8adcbd50e0abfc
kleitoncls <kleitoncls@hotmail.com>
2019-02-21 21:06:13 -0300

    aula do adonias de test

---
+++ src/ExerciciosRevisao/Ex02.java
@@ -0,0 +1,18 @@
+package ExerciciosRevisao;
+
+public class Ex02 {
+    public static void main(String[]args){
+        int totalFolha=100;
+        double resultado;
+        if(totalFolha<=100){
+            resultado=totalFolha*0.25;
+        }else{
+            resultado=totalFolha*0.20;
+        }
+    System.out.println(resultado);
+    }
+
+
+
+}
+
